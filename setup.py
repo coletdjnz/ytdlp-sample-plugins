b4cdeadf31e2f23e6ff3a88817058a3c9eda146c
coletdjnz <coletdjnz@protonmail.com>
2022-11-14 20:06:53 +1300

    fix?

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='ytdlp-sample-plugins',
-    packages=find_namespace_packages(include=[r'ytdlp_plugins\..+'])
+    packages=find_namespace_packages(include=[r'ytdlp_plugins.*'])
 )
